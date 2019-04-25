718a4213adaaef3ceb4f375576951884048f6bf2
Jeroen Harpe <43515079+JeroenHarpe@users.noreply.github.com>
2019-04-25 20:32:17 +0800

    Filling the flip of Personal

--- main.js
+++ main.js
@@ -24,34 +24,59 @@ const personal = document.querySelector('.personal');
 const projects = document.querySelector('.projects');
 const professional = document.querySelector('.professional');
 
+const hPersonal = document.querySelector('.headerPersonal');
+const hProjects = document.querySelector('.headerProjects');
+const hProfessional = document.querySelector('.headerProfessional');
+
+const flipPersonal = document.querySelector('.flipPersonal');
 
 personal.addEventListener('click', openPersonal )
 projects.addEventListener('click', openProjects )
 professional.addEventListener('click', openProfessional )
 
 function openPersonal(){
+  //Animations
+personal.classList.remove('hoverPersonal');
 personal.classList.add('grow');
 projects.classList.add('shrink');
 professional.classList.add('shrink');
 personal.classList.remove('shrink');
 projects.classList.remove('grow');
 professional.classList.remove('grow');
+hPersonal.classList.remove('headerPersonal');
+
+//Flipside
+flipPersonal.style.display = 'flex';
+
+anime({
+  targets: '.myImg',
+  translateX: [-500, 0],
+  delay: 500,
+  rotate: [0, 360],
+  duration: 2500
+ 
+});
+
 }
 
 function openProjects(){
+projects.classList.remove('hoverProjects');
 projects.classList.add('grow');
 personal.classList.add('shrink');
 professional.classList.add('shrink');
 projects.classList.remove('shrink');
 personal.classList.remove('grow');
 professional.classList.remove('grow');
+hProjects.classList.remove('headerProjects')
 }
 
 function openProfessional(){
+professional.classList.remove('hoverProfessional');
 professional.classList.add('grow');
 personal.classList.add('shrink');
 projects.classList.add('shrink');
 professional.classList.remove('shrink');
 projects.classList.remove('grow');
 personal.classList.remove('grow');
+hProfessional.classList.remove('headerProfessional')
 }
